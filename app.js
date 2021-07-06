5bce9b73fb7a618d8717551daf11bc0c340b6988
Martín Ruiz Ptaskin <martinruizptaskin.7@gmail.com>
2021-07-06 10:14:45 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 function act(a,b)
+var orig = '   foo  ';
+console.log(orig.trim()); // 'foo'
+
+// Otro ejemplo de .trim() eliminando el espacio en blanco sólo de un lado.
+
+var orig = 'foo    ';
+console.log(orig.trim()); // 'foo'
